00cbc13b17bf399e06420009cb84d448829b7e81
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2023-11-06 10:25:45 +0530

    Bet App

--- bet_backend/server.js
+++ bet_backend/server.js
@@ -1,37 +1,31 @@
 //IMPORTS
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const cors = require("cors");
-const UserRouter = require("./routes/userRoutes")
+const UserRouter = require("./routes/userRoutes");
 require("dotenv").config();
-const router = require('./routes/betRoutes');
-
+const router = require("./routes/betRoutes");
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use(router);
 
 app.use(UserRouter);
 
-
-
-
 //dbconnection
 mongoose
-    .connect(process.env.dburl, {
-        autoIndex: true
-    })
-    .then(() => {
-        app.listen(5200)
-        console.log("connected");
-    })
-    .catch((err) => {
-        console.log("something went wrong")
-        console.log(err)
-    })
-
-
+  .connect(process.env.dburl, {
+    autoIndex: true,
+  })
+  .then(() => {
+    app.listen(5200);
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log("something went wrong");
+    console.log(err);
+  });
