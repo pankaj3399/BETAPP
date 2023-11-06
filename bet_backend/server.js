c8571ff13de1899a6242290d240cadb83bc9d240
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2023-11-06 10:57:28 +0530

    Bet APP Fix

--- bet_backend/server.js
+++ bet_backend/server.js
@@ -12,30 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: "*" }));
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
 app.use(router);
 
 app.use(userRouter);
@@ -46,7 +22,7 @@ mongoose
     autoIndex: true,
   })
   .then(() => {
-    app.listen(5200);
+    app.listen(process.env.PORT || 5200);
     console.log("connected");
   })
   .catch((err) => {
